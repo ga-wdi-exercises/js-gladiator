176f9e15e29929125173aab5ec88dc60f310293e
brown2206 <brown.angelo.jr@gmail.com>
2016-09-23 12:57:59 -0400

    working on fight

--- arena.js
+++ arena.js
@@ -11,13 +11,34 @@ class Arena{
     document.write("The arena has reached it's maximum capacity for Gladiators!");
     }
   }
-}
+  fight() {
+    var gladiator1 = this.gladiators[0].weapon;
+    var gladiator2 = this.gladiators[1].weapon;
+    if (gladiator1 === "trident" && gladiator2 === "spear"){
+      console.log("Gladiator 1 wins!")
+    }
+    else if (gladiator1 === "spear" && gladiator2 === "club"){
+      console.log("Gladatior 1 wins!")
+    }
+    else if (gladiator1 === "club" && gladiator2 === "trident" ) {
+      console.log("Gladiator 1 wins!")
+    }
+    else if (gladiator1 = gladiator2) {
+      //pop Gladiators with for loop;
+    }
+    else {
+      console.log("Gladiator wins!")
+    }
+    }
+  }
 
 function capitalizeFirstLetter(inStr) {
   return inStr.replace(/\w\S*/g, function(name2)
   { return name2.charAt(0).toUpperCase() + name2.substr(1).toLowerCase();
   });
 }
+
+
 
 //let ga = new Arena("GA")
 //ga.name // "GA"
