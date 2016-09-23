5bd2b1b3a8a3ca9db19dd65890a9630aa82ca008
Mike Flores <michaelprflores@gmail.com>
2016-09-23 13:49:35 -0400

    Gladiators fight and there can be only one winner

--- gladiator.js
+++ gladiator.js
@@ -1,3 +1,4 @@
+
 class Gladiator {
     constructor(name,weapon){
         this.name = name
@@ -6,9 +7,6 @@ class Gladiator {
         } else {
             throw new Error("That weapon is now allowed in this arena.")
         }
-        // if (["Spear", "Trident", "Club"].includes(weapon)){
-        //     this.weapon = weapon
-        // }
     }
 }
 
@@ -24,9 +22,38 @@ class Arena {
             throw new Error("There can be only two.")
         }
     }
+ 
+    fight(gladiatorOne, gladiatorTwo){
+        if (gladiatorOne.weapon == "Trident" && gladiatorTwo.weapon == "Spear"){
+            console.log( gladiatorOne.name + " used the " + gladiatorOne.weapon + " to win!")
+        }
+        else if (gladiatorOne.weapon == "Spear" && gladiatorTwo.weapon == "Club") {
+            console.log( gladiatorOne.name + " used the " + gladiatorOne.weapon + " to win!")
+        } 
+        else if (gladiatorOne.weapon == "Club" && gladiatorTwo.weapon == "Trident") {
+            console.log( gladiatorOne.name + " used the " + gladiatorOne.weapon + " to win!")
+        }
+        else {
+            console.log( gladiatorTwo.name + " wins!")
+        }
+    }
 }
 
 var max = new Gladiator("Maximus","Trident")
 var titus = new Gladiator("Titus","Spear")
 var andronicus = new Gladiator("Andronicus","Club")
 var colosseum = new Arena("Colosseum")
+
+// Test cases
+// colosseum.addGladiator(max)
+// colosseum.addGladiator(titus)
+
+// colosseum.addGladiator(titus)
+// colosseum.addGladiator(andronicus)
+
+// colosseum.addGladiator(andronicus)
+// colosseum.addGladiator(max)
+
+
+
+
