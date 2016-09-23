d91e51bc3632e03c5cc506b4a341bd23de8bc0ee
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-23 13:42:17 -0400

    pt. 1, pt. 2 question 3

---
+++ gladiator.js
@@ -0,0 +1,9 @@
+class Gladiator{
+  constructor(name, weapon){
+    this.name = name;
+    this.weapon = weapon;
+  }
+}
+const rae = new Gladiator("Rae", "spear");
+const earth = new Gladiator("earth", "club");
+const moon = new Gladiator("moon", "trident");
