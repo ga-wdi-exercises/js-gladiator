85aa6550935cebe75b7f7d8c39c8eb763d1d6329
Erica Norby <ericanorby@gmail.com>
2017-02-03 10:40:49 -0500

    added gladiator class, throws error if weapon choice is not valid

---
+++ gladiator.js
@@ -0,0 +1,14 @@
+class Gladiator {
+  constructor(name, weapon){
+    if (weapon == "Spear" || weapon == "Club" || weapon == "Trident"){
+      this.name = name
+      this.weapon = weapon
+    }
+    else {
+      throw new Error("Must choose a valid weapon.");
+    }
+  }
+}
+
+let erica = new Gladiator("Erica","Trident");
+let eva = new Gladiator("Eva","Banana");
