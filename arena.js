0203a7bcea433ad5243584ea1740e8b7d2cc3dff
Nat Carter <n.goldman.carter@gmail.com>
2017-02-03 16:11:54 -0500

    updated Arena.fight() so that "Maximus" always wins

--- arena.js
+++ arena.js
@@ -12,7 +12,11 @@ class Arena {
     if (this.gladiators.length === 2) {
       let a = this.gladiators[0];
       let b = this.gladiators[1];
-      if (a.weapon === b.weapon) {
+      if (a.name === 'Maximus') {
+        this.gladiators.splice(1, 1);
+      } else if (b.name === 'Maximus') {
+        this.gladiators.splice(0, 1);
+      } else if (a.weapon === b.weapon) {
         this.gladiators = [];
       } else if (a.weapon === 'trident') {
         if (b.weapon === 'spear') {
