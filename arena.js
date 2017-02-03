b42cff4f9154c63678af8050fe1b1bcee882695c
Elena Santana <fanelenan7@gmail.com>
2017-02-03 17:03:07 -0500

    splicing the loser out

--- arena.js
+++ arena.js
@@ -12,10 +12,13 @@ class Arena {
     var gladiator1 = this.gladiators[0].weapon
     var gladiator2 = this.gladiators[1].weapon
       if (gladiator1 === "Trident" && gladiator2 === "Spear") {
+        this.gladiators.splice(1)
         console.log("Trident wins!")
       } else if (gladiator1 === "Spear" && gladiator2 === "Club") {
+        this.gladiators.splice(1)
         console.log("Spear wins!")
       } else if (gladiator1 === "Club" && gladiator2 === "Trident") {
+        this.gladiators.splice(1)
         console.log("Club wins!")
       } else {
         console.log("Gladiators killed each other!")
