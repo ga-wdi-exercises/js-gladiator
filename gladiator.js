7ccb49c78e5ecb2ac74489b17330e98ce5fe5913
Nat Carter <n.goldman.carter@gmail.com>
2017-02-03 15:22:30 -0500

    added weapon validation to Gladiator class

--- gladiator.js
+++ gladiator.js
@@ -1,5 +1,9 @@
 class Gladiator {
   constructor(name, weapon) {
+    let weapons = ['spear', 'club', 'trident'];
+    if (weapons.indexOf(weapon) === -1) {
+      throw `Sorry, we're all out of ${weapon}s. Please grab a spear, a club, or a trident.`
+    }
     this.name = name;
     this.weapon = weapon;
   }
