27477c50196535e0c780791d96d45d226233e772
Eva Lai <evalai.dc@gmail.com>
2017-02-03 10:52:00 -0500

    capitalize first letter in arena

---
+++ arena.js
@@ -0,0 +1,9 @@
+class Arena {
+  constructor(name){
+    // capitalize the first letter of arena
+  this.name = name.charAt(0).toUpperCase() + name.slice(1)
+  }
+}
+
+
+let twilight = new Arena('twilight')
