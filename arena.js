e820d49389dc3415c383d32de886db45a5fce704
Samuel Clements <sdkclements@gmail.com>
2017-02-03 16:27:04 -0500

    started on the bonues

--- arena.js
+++ arena.js
@@ -23,23 +23,55 @@ class Arena {
             alert("to many dudes in this piece")
         }
     }
-    fight() {
-    	if (this.gladiators.length = 2){
-    		let fighterOneWeapon = this.gladiators[0].weapon
-    		let fighterTwoWeapon = this.gladiators[1].weapon
-    		console.log(fighterOneWeapon)
-    	}
+    removeGladiator(name) {
+            this.gladiators.remove(name)
+        }
+        // fight() {
+        //     if (this.gladiators.length = 2) {
+        //         let oneWeap = this.gladiators[0].weapon
+        //         let twoWeap = this.gladiators[1].weapon
+        //         if (oneWeap == twoWeap) {
+        //             this.gladiators = []
+        //             console.log('both are dead')
+        //         } else if (oneWeap === spear) {
+        //             if (twoWeap === trident) {
+        //                 console.log('player two wins')
+        //                     //remove player 1
+        //             }
+        //             if (twoWeap === club) {
+        //                 console.log('player One Wins')
+        //                 //remove player 2
+        //             }
 
-    }
-    	
+    //         } else if (oneWeap === trident) {
+    //             if (twoWeap === spear) {
+    //                 console.log('player one wins')
+    //                     //remove player 2
+    //             }
+    //             if (twoWeap === club) {
+    //                 console.log('player two Wins')
+    //                 //remove player 1
+    //             }
+
+    //         } else if (oneWeap === club) {
+    //             if (twoWeap === trident) {
+    //                 console.log('player two wins')
+    //                     //remove player 1
+    //             }
+    //             if (twoWeap === spear) {
+    //                 console.log('player One Wins')
+    //             }
+
+    //         }
+
 }
+
+
 var max = new Gladiator('Sam', 'spear')
-var george = new Gladiator('george','trident')
+var george = new Gladiator('george', 'trident')
 var bigOne = new Arena("theatreofentertainment")
 console.log(bigOne)
 bigOne.addGladiator(max)
 bigOne.addGladiator(george)
 console.log(bigOne.gladiators[0].weapon)
-bigOne.fight()
-
-    
+bigOne.removeGladiator(sam)
