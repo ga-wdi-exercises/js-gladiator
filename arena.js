bf02e7025eeafa5e6c7689dae332442315bffbad
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-03 17:09:05 -0500

    further fixes

--- arena.js
+++ arena.js
@@ -9,17 +9,30 @@ class Arena {
     } else { console.log('already combatants there!')}
   }
   fight() {
-    let a = this.gladiators[0].weapon
-    let b = this.gladiators[1].weqpon
-    if (((a=='spear')&&(b=='club'))||((a=='club')&&(b=='trident'))||((a=='trident')&&(b=='spear'))) {
+    var a = this.gladiators[0].weapon
+    var b = this.gladiators[1].weapon
+    if (a == 'maximus'){
       this.gladiators.splice(1,1)
     }
-    else if (((b=='spear')&&(a=='club'))||((b=='club')&&(a=='trident'))||((b=='trident')&&(a=='spear'))) {
+    else if (b == 'maximus'){
+      this.gladiators.splice(0,1)
+    }
+    else if ((((a=='spear')&&(b=='club'))||((a=='club')&&(b=='trident')))||((a=='trident')&&(b=='spear'))) {
+      this.gladiators.splice(1,1)
+    }
+    else if ((((b=='spear')&&(a=='club'))||((b=='club')&&(a=='trident')))||((b=='trident')&&(a=='spear'))) {
       this.gladiators.splice(0,1)
     }
     else {
-      this.gladiators.splice(0,1)
-      this.gladiators.splice(1,1)
+      this.gladiators.splice(0,2)
     }
+  }
+  remove(name) {
+    var self = this
+    self.gladiators.forEach(function(glad) {
+      if (name==glad) {
+        self.gladiators.splice(self.gladiators.indexOf(glad),1);
+      }
+    })
   }
 }
