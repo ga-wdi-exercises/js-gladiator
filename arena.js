18660c65df98700152b463486d87f135d752f4ba
Aaron <andrews_aaron@bah.com>
2017-02-03 12:27:35 -0500

    final commit couldn't finish it in time

---
+++ arena.js
@@ -0,0 +1,16 @@
+
+
+
+
+class Arena{
+  constructor(name){
+    this.name = name
+    this.colosseum = colosseum
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+}
+var colosseum = new Arena("megalopolis")
+var colosseum = new Arena("colosseum")
+
+
+// couldnt get the function to capitalize the first letter and couldn't find a way around it.  I worked with Chris and we were both stumped by it.
---
+++ gladiator.js
@@ -0,0 +1,10 @@
+class Gladiator{
+  constructor(name, weapon){
+    this.name = name
+    this.weapon = weapon
+  }
+  weaponCheck (){
+    if (this.weapon != spear || this.weapon != club || this.weapon != trident)
+    console.log()
+  }
+}
