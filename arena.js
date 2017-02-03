21a7357bf4be27d05d600bbc4682f6790fe3654a
Wes <wes.coffay@gmail.com>
2017-02-03 16:19:07 -0500

    basic functions intact

---
+++ arena.js
@@ -0,0 +1,48 @@
+class Arena {
+  constructor (name){
+    this.name = name.charAt(0).toUpperCase()+name.slice(1)
+    this.gladiators = []
+  }
+  addGladiator(gladiator) {
+    if (this.gladiators.length<2){
+      this.gladiators.push(gladiator)
+    } else {
+      console.log('Both combatants already here')
+    }
+  }
+  battle() {
+    if (this.gladiators.length === 2){
+      if (this.gladiators[0].weapon === this.gladiators[1].weapon){
+        console.log("Its a draw. Both contestants go free")
+        this.gladiators = []
+      } else if (this.gladiators[0].weapon === "squirtle" && this.gladiators[1].weapon === "charmander") {
+        // console.log(`${this.gladiators[0]} wins!`)
+        this.gladiators.pop()
+        console.log(this.gladiators)
+      } else if (this.gladiators[0].weapon === "charmander" && this.gladiators[1].weapon === "bulbasaur"){
+        // console.log(`${this.gladiators[0]} wins!`)
+        this.gladiators.pop()
+        console.log(this.gladiators)
+      } else if (this.gladiators[0].weapon === "bulbasaur" && this.gladiators[1].weapon === "squirtle"){
+        // console.log(`${this.gladiators[0]} wins!`)
+        this.gladiators.pop()
+        console.log(this.gladiators)
+      } else if (this.gladiators[0].weapon === "bulbasaur" && this.gladiators[1].weapon === "charmander"){
+        this.gladiators.splice(0, 1)
+        console.log(this.gladiators)
+      } else if (this.gladiators[0].weapon === "squirtle" && this.gladiators[1].weapon === "bulbasaur"){
+        this.gladiators.splice(0, 1)
+        console.log(this.gladiators)
+      } else if (this.gladiators[0].weapon === "charmander" && this.gladiators[1].weapon === "squirtle"){
+        this.gladiators.splice(0, 1)
+        console.log(this.gladiators)
+      }
+    }
+  }
+}
+var pokedome = new Arena('pokedome')
+console.log(pokedome.name)
+console.log(pokedome.gladiators)
+
+
+// console.log('success!')
---
+++ gladiator.js
@@ -0,0 +1,13 @@
+class Gladiator {
+  constructor(name, weapon){
+    this.name = name
+    this.weapon = weapon
+  }
+}
+
+var agron = new Gladiator('Agron', 'charmander')
+var crixus = new Gladiator('Crixus', 'bulbasaur')
+var nasir = new Gladiator('Nasir', 'squirtle')
+
+console.log(agron)
+console.log(crixus)
