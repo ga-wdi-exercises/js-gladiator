a3974c14a8c79df48326cbab3475c2635dd706cf
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-03 16:15:42 -0500

    mistake fixes

--- arena.js
+++ arena.js
@@ -4,7 +4,7 @@ class Arena {
     this.gladiators = []
   }
   addGladiator(newGlad) {
-    if (this.gladiators.length < 3) {
+    if (this.gladiators.length < 2) {
       this.gladiators.push(newGlad)
     } else { console.log('already combatants there!')}
   }
@@ -12,10 +12,10 @@ class Arena {
     let a = this.gladiators[0].weapon
     let b = this.gladiators[1].weqpon
     if (((a=='spear')&&(b=='club'))||((a=='club')&&(b=='trident'))||((a=='trident')&&(b=='spear'))) {
-      this.gladiators.splice(0,1)
+      this.gladiators.splice(1,1)
     }
     else if (((b=='spear')&&(a=='club'))||((b=='club')&&(a=='trident'))||((b=='trident')&&(a=='spear'))) {
-      this.gladiators.splice(1,1)
+      this.gladiators.splice(0,1)
     }
     else {
       this.gladiators.splice(0,1)
--- gladiator.js
+++ gladiator.js
@@ -1,7 +1,6 @@
 class Gladiator {
   constructor(nam, weapon) {
     this.name = nam.charAt(0).toUpperCase() + nam.slice(1)
-    this.weapon = weapon
+    this.weapon = ((weapon=='spear')||(weapon=='trident')||(weapon=='club'))? weapon:undefined
   }
-
 }
