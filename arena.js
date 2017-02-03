8a90a1ed4734b5537e5cc55b222d5e04751a086d
Nat Carter <n.goldman.carter@gmail.com>
2017-02-03 15:04:25 -0500

    added addGladiator method to Arena class

--- arena.js
+++ arena.js
@@ -3,4 +3,7 @@ class Arena {
     this.name = name[0].toUpperCase() + name.slice(1).toLowerCase();
     this.gladiators = [];
   }
+  addGladiator(gladiator) {
+    this.gladiators.push(gladiator);
+  }
 }
