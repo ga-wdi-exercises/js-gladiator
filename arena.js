1b2ca9b2c555a9f84ae826641f27d66d256dd173
Nat Carter <n.goldman.carter@gmail.com>
2017-02-03 15:52:57 -0500

    added fight() method to Arena class; fixed weapon validation in Gladiator class

--- arena.js
+++ arena.js
@@ -8,4 +8,31 @@ class Arena {
       this.gladiators.push(gladiator);
     }
   }
+  fight() {
+    if (this.gladiators.length === 2) {
+      let a = this.gladiators[0];
+      let b = this.gladiators[1];
+      if (a.weapon === b.weapon) {
+        this.gladiators = [];
+      } else if (a.weapon === 'trident') {
+        if (b.weapon === 'spear') {
+          this.gladiators.splice(1, 1);
+        } else {
+          this.gladiators.splice(0, 1);
+        }
+      } else if (a.weapon === 'spear') {
+        if (b.weapon === 'club') {
+          this.gladiators.splice(1, 1);
+        } else {
+          this.gladiators.splice(0, 1);
+        }
+      } else {
+        if (b.weapon === 'trident') {
+          this.gladiators.splice(1, 1);
+        } else {
+          this.gladiators.splice(0, 1);
+        }
+      }
+    }
+  }
 }
--- gladiator.js
+++ gladiator.js
@@ -1,5 +1,6 @@
 class Gladiator {
   constructor(name, weapon) {
+    weapon = weapon.toLowerCase();
     let weapons = ['spear', 'club', 'trident'];
     if (weapons.indexOf(weapon) === -1) {
       throw `Sorry, we're all out of ${weapon}s. Please grab a spear, a club, or a trident.`
