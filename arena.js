46ade064c9bfd4df3674342f9e7bfb4738f4d523
Caroline Chen <carolinecaroline@gmail.com>
2017-03-31 10:47:05 -0400

    can add gladiator to arena

--- arena.js
+++ arena.js
@@ -1,6 +1,10 @@
 class Arena {
-	constructor(name){
+	constructor(name, gladiators){
 		//toUpperCase will make every letter a capital, so we have to target just the first letter, then add on the rest
 		this.name = name.charAt(0).toUpperCase() + name.slice(1);
+		this.gladiators = [];
+	}
+	addGladiator(newGladiator){
+		this.gladiators.push(newGladiator);
 	}
 }
