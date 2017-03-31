3826bf2d933085ca68f36dd54024a32d79466083
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-03-31 11:56:34 -0400

    First commit

---
+++ arena.js
@@ -0,0 +1,9 @@
+class Arena {
+  constructor(place){
+  this.place = place;}
+}
+var pan = new Arena("Panic at the Parthanon");
+var han = new Arena("Horror at the Hanging Gardens");
+var sta = new Arena("Stomping at the Stadium");
+var bea = new Arena("Bumrush the Beach");
+var col = new Arena("Clash at the Coliseum");
---
+++ gladiator.js
@@ -0,0 +1,10 @@
+new Weapon
+class Weapon {
+  constructor(weapon){
+  this.weapon = weapon;}
+}
+var net = new Weapon("Net");
+var kic = new Weapon("Kicker");
+var clu = new Weapon("Club");
+var mac = new Weapon("Mace");
+var cla = new Weapon("Claw");
