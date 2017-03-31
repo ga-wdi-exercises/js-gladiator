caff80a1099ac0f346d2ada1ac6d489b25b0495a
Whitney <whitney.purdum@gmail.com>
2017-03-31 11:55:21 -0400

    final commit

--- gladiator.js
+++ gladiator.js
@@ -1,8 +1,8 @@
 
 
 class Gladiator {
-	constructor(name, weapon){
-		this.name = name;
+	constructor(warrior, weapon){
+		this.warrior = warrior;
 		this.weapon = weapon;
 	}
 
@@ -17,13 +17,40 @@ class Gladiator {
 const Whitney = new Gladiator("Whitney", "knife");
 const Andrew = new Gladiator("Andrew", "knife");
 
-class Arena extends Gladiator {
+class Arena {
 	constructor(name){
-		super(name, weapon);
+
 		this.name = name.toUpperCase();
+		this.gladiators = [];
 	}
+			addGladiator(Gladiator) {
+				if (this.gladiators.length < 2) {
+				this.gladiators.push(Gladiator)
+				} else  if (this.gladiators.length > 2) {
+				console.log("Arena full. Come back later")
+				}
+			};
 
-	
-	};
+			fight() {
+				if (this.gladiators.length = 2) {
+					console.log("fighting!")
+					console.log(this.gladiators[0].weapon)
+					console.log(this.gladiators[1].weapon)
 
-const myArena = new Arena("babel");
+				}
+			}
+
+
+		};
+const myArena = new Arena("babel")
+console.log("Arena being created!");
+
+const newestArena = new Arena("field");
+
+const Alex = new Gladiator("Sarah", "cannon");
+
+const Sam = new Gladiator("sam", "spear");
+
+const Tim = new Gladiator("tim", "trident");
+
+
