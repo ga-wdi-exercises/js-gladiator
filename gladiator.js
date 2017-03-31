9568b4bee51e5f671e0717947820aacc59c1ab2c
Erin <erinedawson@gmail.com>
2017-03-31 10:17:10 -0400

    add gladiator class

---
+++ gladiator.js
@@ -0,0 +1,10 @@
+class Gladiator {
+  constructor(name, weapon){
+    this.name = name;
+    this.weapon = weapon;
+  }
+}
+
+var max = new Gladiator("Maximus","Trident")
+console.log(max.name)
+console.log(max.weapon)
