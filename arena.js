050534529f5a9f5774ba36634edbf1bb77c1278f
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-31 11:26:19 -0400

    create classes for arena and gladiator classes and create a function to add a gladiator to an array

---
+++ arena.js
@@ -0,0 +1,18 @@
+class Arena {
+  constructor(arenaName){
+    this.arenaName = arenaName.toUpperCase();
+    this.gladiators = [];
+  }
+  addGladiator(fighter){
+    this.gladiators.push(fighter);
+    console.log(this.gladiators);
+    if (this.gladiators.length > 2) {
+      this.gladiators.pop();
+      console.log('Too many gladiators')
+      console.log(this.gladiators.length)
+    }
+  }
+}
+
+var colosseum = new Arena("Colosseum");
+var myLivingRoom = new Arena("My Living Room");
