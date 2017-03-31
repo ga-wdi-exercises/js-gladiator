f6e0c6de5680060c3cd841dae10a32ce2b110834
Jonathan Wright <jonwright.va@gmail.com>
2017-03-31 10:20:49 -0400

    gladiator class created. Bonus not complete

---
+++ gladiator.js
@@ -0,0 +1,12 @@
+class Gladiator {
+  constructor(name, weapon) {
+    this.name = name.charAt(0).toUpperCase() + name.slice(1);
+    this.weapon = weapon.charAt(0).toUpperCase() + weapon.slice(1);;
+  }
+  // if (this.weapon !== "Spear" || this.weapon !== "Club" || this.weapon !== "Trident" ) {
+  //   throw `Invalid weapon, must be a Spear, Club, or Trident`
+  // }
+}
+
+let joe = new Gladiator("jame", "spear");
+let jon = new Gladiator('jon', 'cheese');
