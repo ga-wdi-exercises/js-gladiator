e89e0b588ea206f611b891f87a3783da99639b2d
Caroline Chen <carolinecaroline@gmail.com>
2017-03-31 11:52:28 -0400

    add fight logic

--- arena.js
+++ arena.js
@@ -4,9 +4,44 @@ class Arena {
 		this.name = name.charAt(0).toUpperCase() + name.slice(1);
 		this.gladiators = [];
 	}
-	addGladiator(newGladiator){
+	addGladiator(Gladiator){
 		if (this.gladiators.length < 2){
-			this.gladiators.push(newGladiator);
+			this.gladiators.push(Gladiator);
+		}
+	}
+	fight(){
+		if (this.gladiators.length = 2){
+
+
+
+			if (colosseum.gladiators[0].weapon == "Trident" && colosseum.gladiators[1].weapon == "Spear" ){
+				alert ("trident beats spear so first guy wins");
+			} else if (colosseum.gladiators[0].weapon == "Trident" && colosseum.gladiators[1].weapon == "Club" ){
+				alert ("club beats trident so second guy wins");
+
+
+
+
+
+
+			} else if (colosseum.gladiators[0].weapon == "Spear" && colosseum.gladiators[1].weapon == "Club"){
+		
+				alert ("spear beats club so first guy wins");
+			} else if (colosseum.gladiators[0].weapon == "Spear" && colosseum.gladiators[1].weapon == "Trident"){
+				alert ("trident beats spear so second guy wins");
+			
+
+
+			} else if (colosseum.gladiators[0].weapon == "Club" && colosseum.gladiators[1].weapon == "Trident"){
+			
+				alert ("club beats trident so first guy wins");
+			} else if (colosseum.gladiators[0].weapon == "Club" && colosseum.gladiators[1].weapon == "Spear") {
+				alert ("spear beats club so second guy wins");
+			}
+
+
+
+
 		}
 	}
 }
