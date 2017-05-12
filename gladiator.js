124260eb165849d0f538956708d05543b39ac070
Bao Cong Pham <BaoPham92@yahoo.com>
2017-05-12 11:56:29 -0400

    gladiator

---
+++ gladiator.js
@@ -0,0 +1,19 @@
+class Gladiator {
+		constructor(name, weapon){
+		this.name = name;
+		this.weapon = weapon;
+		}
+		
+}
+
+const glad1 = new Gladiator("Tarik", "Weapon of choice: Can of beans and a slingshot.");
+	console.log(glad1.name)
+	console.log(glad1.weapon)
+
+const glad2 = new Gladiator("Deli Clerk", "Weapon of choice: Steak & Cheese sandwhiches.")
+	console.log(glad2.name)
+	console.log(glad2.weapon)
+
+const glad3 = new Gladiator("Tim Chase. \"Teh evil twin\"", "Weapon of choice: A white board, sharpy, hell of a dangerous mind.")
+	console.log(glad3.name)
+	console.log(glad3.weapon)
