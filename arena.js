86359cd61fe4c787a9422948725d67992ec14c12
tedlayea <tedlayea@gMAIL.com>
2017-05-12 11:47:10 -0400

    first modification

---
+++ arena.js
@@ -0,0 +1,36 @@
+class Arena {
+  constructor(name){
+    this.name=name.charAt(0).toUpperCase() + name.slice(1);
+    this.gladiators=[];
+    console.log(" Welcome to "+name+"!!!")
+  }
+  addGladiator(name){
+      this.name=name;
+      this.gladiators.push(this.name);
+      console.log(this.name+" added!!!")
+      console.log(this.gladiators.length);
+
+    if (this.gladiators.length==2){
+      return ("You already have two players");
+    }
+  }
+
+fight(){
+  if (this.gladiators.length===2){
+    if (this.gladiators[0]==this.gladiators[1]){
+       delete this.gladiators[0];
+       delete this.gladiators[1];
+      }
+    else{
+      // Trident beats Spear
+      // Spear beats Club
+      // Club beats Trident
+    }
+  }
+  else{
+console.log("You have no enough players to start the game");
+  }
+}
+
+
+}
