3be82072d4dcc2aa514a7735caf85acacc18b5f3
Tim Chase <tchase4544@gmail.com>
2017-05-12 10:57:07 -0400

    arena + fight

--- arena.js
+++ arena.js
@@ -2,6 +2,7 @@ class Arena{
 	constructor(name){
 
 		this.name = name
+		this.cap(name)
 		this.gladiators = []
 	}
 	cap(name){
@@ -12,5 +13,46 @@ class Arena{
 
 	addGladiator(fighter){
 		this.gladiators.push(fighter)
+		if (this.gladiators.length > 2) {
+			this.gladiators.shift()
+		}
+	}
+	fight(){
+
+		let fighter1 = this.gladiators[0]
+		let fighter2 = this.gladiators[1]
+
+	if (this.gladiators.length === 2) {
+		if (fighter1.weapon === fighter2.weapon) {
+			console.log("Both fighters were eliminated...")
+		}else if (fighter1.weapon === "Spear") {
+
+			if (fighter2.weapon === "Club") {
+				this.gladiators.pop()
+			}else {
+				this.gladiators.shift()
+			}
+		}else if (fighter1.weapon === "Club") {
+			if (fighter2.weapon === "Trident") {
+				this.gladiators.pop()
+			}else{
+				this.gladiators.shift()
+			}
+
+		}else if (this.fighter1.weapon === "Trident") {
+			if (this.fighter2.weapon === "Spear") {
+				this.gladiators.pop()
+			}else{
+				this.gladiators.shift()
+			}
+		}else{
+			return "error"
+		}
+
+	}else{
+		console.log(`${this.gladiators[0]} needs an opponet!`)
+	}
 	}
 }
+
+const colli = new Arena("colosseum")
--- gladiator.js
+++ gladiator.js
@@ -2,7 +2,19 @@ class Gladiator {
 	constructor(name,weapon){
 		this.name = name
 		this.weapon = weapon
+		this.weaponsCheck(weapon)
+	}
+	weaponsCheck(weapon){
+		const possibleWeapons = ["Spear","Club","Trident"]
+		if (possibleWeapons.includes(weapon)) {
+			this.weapon = weapon
+			return
+		}else{
+			this.weapon = "Club"
+			alert("Error! not a valid weapon \n you've been given a Club")
+		}
 	}
 }
-
-//includes(spear) || includes(club) || includes(trident)
+const max = new Gladiator("Maximus","Spear")
+const comi = new Gladiator("Commidus" , "Club")
+//   arry.includes(input)
