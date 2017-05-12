f8445f11d5fb74e094ca873ce7c6cf5acebd3e30
Tim Chase <tchase4544@gmail.com>
2017-05-12 09:31:22 -0400

    basic arena & can add gladiators

--- arena.js
+++ arena.js
@@ -1,3 +1,16 @@
 class Arena{
+	constructor(name){
 
+		this.name = name
+		this.gladiators = []
+	}
+	cap(name){
+			let firstL = name.charAt(0).toUpperCase()
+			let rest = name.slice(1)
+			this.name = firstL + rest
+		}
+
+	addGladiator(fighter){
+		this.gladiators.push(fighter)
+	}
 }
