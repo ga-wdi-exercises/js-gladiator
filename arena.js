7f91c82bead3b2d6c3bc1dd8e15dd7fbaf67d8cc
Robert Lowe <RobLowe1992@Yahoo.com>
2017-05-12 09:53:18 -0400

    Gave arena class a gladiator array and an addGladiator method

--- arena.js
+++ arena.js
@@ -1,5 +1,9 @@
 class Arena{
     constructor(name='New Arena'){
-        this.name = name;
+        this.name = name.charAt(0).toUpperCase() + name.slice(1);
+        this.gladiators = [];
+    }
+    addGladiator(gladiator) {
+        this.gladiators.push(gladiator);
     }
 }
