b9b77e2c169075b7388bbeecfd19b88ff8150bb8
Scott Pierce <scottpiercew@gmail.com>
2017-05-12 11:18:54 -0400

    Part one bonus complete

---
+++ arena.js
@@ -0,0 +1,25 @@
+class Arena {
+  constructor(name, gladiators){
+    this.name = name.charAt(0).toUpperCase() + name.slice(1);
+    this.gladiators = [];
+}
+  addGladiator(gladiators){
+    this.gladiators.unshift(gladiators);
+    if (this.gladiators.length > 2) {
+        this.gladiators.pop();
+      }
+    }
+  fight(){
+    if (this.gladiators[0].weapon == "Trident" && this.gladiators[1].weapon == "Spear") {
+      this.gladiators.pop()
+    } else if (this.gladiators[0].weapon == "Spear" && this.gladiators[1].weapon == "Club") {
+      this.gladiators.pop()
+    } else if (this.gladiators[0].weapon == "Club" && this.gladiators[1].weapon == "Trident") {
+      this.gladiators.pop()
+    } else if (this.gladiators[0].weapon == this.gladiators[1].weapon) {
+      this.gladiators.pop()
+      this.gladiators.shift()
+    }
+  }
+
+}
