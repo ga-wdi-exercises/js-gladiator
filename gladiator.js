8c3c756dc3a8b945eb0c388f0f03c963b884c623
USMAN AHMED <usmanahmed090@gmail.com>
2017-05-12 13:58:00 -0400

    incomplete

---
+++ gladiator.js
@@ -0,0 +1,10 @@
+class gladiator(name, weapon){
+  this.name: name;
+  this.weapon: weapon;
+
+
+
+  var max = new Gladiator("Maximus", "Trident")
+  console.log(max.name) // "Maximus"
+  console.log(max.weapon) // "Trident"
+}
