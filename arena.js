6084781fa829a447b719f933cf40056bc6fbd482
ANTWANA WADLINGTON <antwana.wadlington@gmail.com>
2017-05-12 11:03:26 -0400

    AntwanaW Lab

---
+++ arena.js
@@ -0,0 +1,24 @@
+class Arena{
+  constructor(name){
+    this.name = name
+    this.gladiators=[]
+  }
+  addGladiator(Gladiator){
+    this.gladiators.push(max);
+ }
+
+}
+var colosseum = new Arena("Colosseum")
+console.log(colosseum.name)
+
+var colosseum = new Arena("Megalopolis")
+console.log(colosseum.name)
+
+var max = new Gladiator("Maximus", "Trident")
+var colosseum = new Arena("Colosseum")
+console.log(colosseum.gladiators)
+
+
+
+
+//class Megalopolis extends
