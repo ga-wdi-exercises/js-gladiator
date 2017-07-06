cc9f706ef9813c834b3bc923512411d6b40dc71d
Kendall <adkins.kendall90@gmail.com>
2017-07-06 17:01:06 -0400

    finished basic

--- arena.js
+++ arena.js
@@ -12,11 +12,51 @@ class Arena {
       return this.fighters.push(fighter)
     }
   }
+  fight () {
+    var weap1 = this.fighters[0].weapon
+    var weap2 = this.fighters[1].weapon
+    if (weap1 === 'trident') {
+      switch (weap2) {
+        case 'trident':
+          this.fighers = [] // same weapon -- tie remove both
+          break
+        case 'spear':
+          this.fighters.pop() // trident beats spear remove fighter 2
+          break
+        case 'club':
+          this.fighters.shift() // club beats trident remove fighter 1
+          break
+      }
+    } else if (weap1 === 'club') {
+      switch (weap2) {
+        case 'club':
+          this.fighers = [] // same weapon -- tie remove both
+          break
+        case 'spear':
+          this.fighters.shift() // spear beats club remove fighter 1
+          break
+        case 'trident':
+          this.fighters.pop() // club beats trident remove fighter 2
+          break
+      }
+    } else if (weap1 === 'spear') {
+      switch (weap2) {
+        case 'spear':
+          this.fighers = [] // same weapon -- tie remove both
+          break
+        case 'club':
+          this.fighters.pop() // spear beats club remove fighter 1
+          break
+        case 'trident':
+          this.fighters.shift() // trident beats spear remove fighter 2
+          break
+      }
+    }
+  }
 }
 
-
-if (this.weapon !== 'spear' || this.weapon !== 'club' || this.weapon !== 'trident') {
-  alert(`Your ${this.weapon} is invalid, please change your weapon before entering the arena or you will be killed`)
-} else {
-  alert(`Your ${this.weapon} is valid, not the one I'd choose, but good luck`)
-}
+// if (this.weapon !== 'spear' || this.weapon !== 'club' || this.weapon !== 'trident') {
+//   alert(`Your ${this.weapon} is invalid, please change your weapon before entering the arena or you will be killed`)
+// } else {
+//   alert(`Your ${this.weapon} is valid, not the one I'd choose, but good luck`)
+// }
--- gladiator.js
+++ gladiator.js
@@ -4,10 +4,5 @@ class Gladiator {
   constructor (name, weapon) {
     this.name = name
     this.weapon = weapon
-    // if (this.weapon !== 'spear' || this.weapon !== 'club' || this.weapon !== 'trident') {
-    //   alert(`Your ${this.weapon} is invalid, please change your weapon before entering the arena or you will be killed`)
-    // } else {
-    //   alert(`Your ${this.weapon} is valid, not the one I'd choose, but good luck`)
-    // }
   }
 }
