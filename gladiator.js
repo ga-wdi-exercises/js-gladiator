1ff70375a5dc2d1a6e236180be530b6175c8e367
Liz Jewell <eghjewell@gmail.com>
2017-07-06 16:39:43 -0400

    update JS

---
+++ gladiator.js
@@ -0,0 +1,11 @@
+class Gladiator {
+  constructor (name, weapon) {
+    this.name = name
+    if (weapon === 'Spear' || weapon === 'Club' || weapon === 'Trident') {
+      this.weapon = weapon
+    } else {
+      throw 'This is not a valid weapon!'
+    }
+  }
+
+}
