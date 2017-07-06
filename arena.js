7e9ebcbd82b540fc31a1a8195f61d804f5a10ada
Richard Schram <rcschram@yahoo.com>
2017-07-06 17:02:47 -0400

    still working on these gladiators

---
+++ arena.js
@@ -0,0 +1,15 @@
+class Arena {
+  constructor (name) {
+    this.name = name.charAt(0).toUpperCase() + name.slice(1);
+    this.gladiator = [];
+    }
+  }
+
+addGladiator (glad) {
+  if (this.gladiators.length < 2) {
+    this.gladiators.push(glad)
+  }else{
+    console.;log('Too many gladiators in the ring')
+  }
+}
+var colosseum = new Arena ("Verona")
